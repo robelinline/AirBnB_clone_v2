4c18f44cb2ac4ad7ffc14ecb53c691225347ee2b
MirewK <meremehertab@gmail.com>
2024-03-11 14:12:45 -0700

    console

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A method for the models directory"""
+"""__init__ method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
