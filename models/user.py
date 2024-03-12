650d32ed8f0aab35949203bdb41161b03fcc737a
robelinline <62613359+robelinline@users.noreply.github.com>
2024-03-12 20:42:47 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""the total defenition of the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
+    """the whoel Rep of  a User.
 
     Attributes:
-        email (str): the email of the user
-        password (str): the password of the user
-        first_name (str): the first name of the user
-        last_name (str): the last name of the user
+        email (str): email of the user
+        password (str): password of the user
+        first_name (str): first name of the user
+        last_name (str): last name of the user
     """
 
     email = ""
