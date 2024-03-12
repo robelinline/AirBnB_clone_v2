b0673620def3e69fb2ecee8b8a680fa81a90da8e
robelinline <62613359+robelinline@users.noreply.github.com>
2024-03-12 20:20:49 +0300

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/user.py.
+"""Definitions of testing for models/user.py.
 
 Unittest classes:
     TestUser_instantiation
@@ -15,7 +15,7 @@ from models.user import User
 
 
 class TestUser_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the User class."""
+    """the testing of  instantiation of the User class."""
 
     def test_no_args_instantiates(self):
         self.assertEqual(User, type(User()))
@@ -91,7 +91,7 @@ class TestUser_instantiation(unittest.TestCase):
 
 
 class TestUser_save(unittest.TestCase):
-    """Unittests for testing save method of the  class."""
+    """the testing function or save method of the  class."""
 
     @classmethod
     def setUp(self):
@@ -142,7 +142,7 @@ class TestUser_save(unittest.TestCase):
 
 
 class TestUser_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the User class."""
+    """the testing of the to_dict method of the User class."""
 
     def test_to_dict_type(self):
         self.assertTrue(dict, type(User().to_dict()))
