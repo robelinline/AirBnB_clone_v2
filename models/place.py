24fb096b1e0256aa39f894612d85df50f53459c4
robelinline <62613359+robelinline@users.noreply.github.com>
2024-03-12 20:40:17 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""the defenation ogf the Place class."""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represent a place.
+    """the Represent a place.
 
     Attributes:
         city_id (str): The City id.
         user_id (str): The User id.
-        name (str): The name of the place.
+        name (str): name of the place.
         description (str): The description of the place.
-        number_rooms (int): The number of rooms of the place.
-        number_bathrooms (int): The number of bathrooms of the place.
-        max_guest (int): The maximum number of guests of the place.
+        number_rooms (int): The totalr of rooms of the place.
+        number_bathrooms (int): the total of bathrooms of the place.
+        max_guest (int): ma number  of guests of the place.
         price_by_night (int): The price by night of the place.
         latitude (float): The latitude of the place.
         longitude (float): The longitude of the place.
