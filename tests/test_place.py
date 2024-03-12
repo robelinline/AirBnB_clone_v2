e04079cd6b6eb06c452460cc4560a3b48ce5d036
robelinline <62613359+robelinline@users.noreply.github.com>
2024-03-12 20:14:53 +0300

    Update test_place.py

--- tests/test_place.py
+++ tests/test_place.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/place.py.
+"""Defines if the unit tests for models/place.py.
 
 Unittest classes:
     TestPlace_instantiation
@@ -15,7 +15,7 @@ from models.place import Place
 
 
 class TestPlace_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Place class."""
+    """testing instantiation of the Place class."""
 
     def test_no_args_instantiates(self):
         self.assertEqual(Place, type(Place()))
@@ -145,7 +145,7 @@ class TestPlace_instantiation(unittest.TestCase):
 
 
 class TestPlace_save(unittest.TestCase):
-    """Unittests for testing save method of the Place class."""
+    """thefor testing save method of the Place class."""
 
     @classmethod
     def setUp(self):
@@ -196,7 +196,7 @@ class TestPlace_save(unittest.TestCase):
 
 
 class TestPlace_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the Place class."""
+    """particular testing to_dict method of the Place class."""
 
     def test_to_dict_type(self):
         self.assertTrue(dict, type(Place().to_dict()))
