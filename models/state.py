84cc2ee648bc182bd38ec9ed1841f1e7437a798b
robelinline <62613359+robelinline@users.noreply.github.com>
2024-03-12 20:41:43 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines the state class of the user"""
+"""the Defination of the state class of the user"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state class
+    """the whole Rep a state class
 
     Attributes:
         name (str): the state neme of the user
